5e1e7f1a18757b3d584f6cd4a7db296324d84c04
hyerin133 <hyerin8013@gmail.com>
2021-10-26 07:51:46 +0900

    helloworld 출력문 제거

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -3,7 +3,6 @@ package com.kh.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world~");
 		System.out.println("Hello git~");
 		System.out.println("Hello web~");
 
